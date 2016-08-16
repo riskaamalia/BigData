97b5140468ae4160675f8b1c736fb711f0a61c38
Riska <riska.amalia@mediatrac.co.id>
2016-08-16 18:32:12 +0700

    diamond

---
+++ graph-accumulo/src/test/java/codeTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by 1224A on 8/16/2016.
+ */
+public class codeTest {
+    @Test
+    public void testDiamond3() {
+        StringBuffer expected = new StringBuffer();
+        expected.append(" *\n");
+        expected.append("***\n");
+        expected.append(" *\n");
+
+        assertEquals(expected.toString(), code.print(3));
+    }
+
+    @Test
+    public void testDiamond5() {
+        StringBuffer expected = new StringBuffer();
+        expected.append("  *\n");
+        expected.append(" ***\n");
+        expected.append("*****\n");
+        expected.append(" ***\n");
+        expected.append("  *\n");
+
+        assertEquals(expected.toString(), code.print(5));
+    }
+}
